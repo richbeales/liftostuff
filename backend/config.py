44186e26508ebde529f1085491f2084636f39f22
Rich <rich@richbeales.net>
2025-03-05 17:47:15 +0000

    set db conn

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
+    PWD = os.environ.get("POSTGRES_PASSWORD")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:postgres@db:5432/workout_app'
+        'postgresql://lift_admin:%s@lift-postgres:5432/lift_db' % (PWD)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
