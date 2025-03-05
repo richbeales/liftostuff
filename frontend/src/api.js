5bef8aa07686ff777193aa60905b918ea5eab780
Rich <rich@richbeales.net>
2025-03-05 18:35:11 +0000

    frontend mods

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://api.liftostuff.com';
+const API_URL = process.env.REACT_APP_API_URL || 'https://api.liftostuff.com';
 
 export const api = axios.create({
   baseURL: API_URL
